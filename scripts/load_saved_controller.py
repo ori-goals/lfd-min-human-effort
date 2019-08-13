80f5f0d1c5934227e3a39b89bd95c9f4fe68c9c4
marcrigter96 <marcrigter@gmail.com>
2019-08-13 20:11:40 +0100

    try more varied cases

--- scripts/load_saved_controller.py
+++ scripts/load_saved_controller.py
@@ -9,13 +9,13 @@ if __name__ == "__main__" :
 
     for i in range(0, 3):
         sim = Simulation()
-        saved_controller_file = '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/ddpg_bc_testing/demos_0_to_49/rl_attempt_aug11_first50.pkl'
+        saved_controller_file = '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/tuning_problem_domain/high_variation_aug13/demos/2019-08-13-19-39_joystick_teleop242.pkl'
         sim.add_controllers({'ddpg':{}, 'saved_teleop':{'file':saved_controller_file, 'type':'joystick_teleop'}})
-        case_name = 'rl_attempt_aug11'
+        case_name = 'high_variation_aug13'
 
-        for case_number in range(0, 500):
-            if case_number < 50:
+        for case_number in range(0, 800):
+            if case_number < 240:
                 sim.run_new_episode(case_name, case_number/2, controller_type = 'joystick_teleop')
             else:
                 sim.run_new_episode(case_name, case_number/2, controller_type = 'ddpg')
-        sim.save_simulation('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/ddpg_bc_testing/add_q_filter/q_filter_mods/demos_then_rl')
+        sim.save_simulation('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/tuning_problem_domain/high_variation_aug13/demos_then_rl')
--- scripts/save_demos.py
+++ scripts/save_demos.py
@@ -8,9 +8,8 @@ if __name__ == "__main__" :
     sim = Simulation()
     sim.add_controllers({'joystick_teleop':{}})
 
-    case_name = 'rl_attempt_aug11'
-    for case_number in range(50):
+    case_name = 'high_variation_aug13'
+    for case_number in range(301):
         sim.run_new_episode(case_name, case_number, controller_type = 'joystick_teleop')
-        if case_number == 3:
-            sim.save_simulation('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/ddpg_bc_testing/demos_0_to_49')
-    sim.save_simulation('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/ddpg_bc_testing/demos_0_to_49')
+        if (case_number - 1) % 20 == 0:
+            sim.save_simulation('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/initial_tests/tuning_problem_domain/high_variation_aug13/demos')
