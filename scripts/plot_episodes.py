fc84c7f3fb5b7cde2c966901647618344ac7262f
marcrigter96 <marcrigter@gmail.com>
2019-12-09 15:44:23 +0000

    add limited demos plot

--- scripts/plot_episodes.py
+++ scripts/plot_episodes.py
@@ -8,7 +8,7 @@ def plot_length_param():
     plot_length_param_estimation(known_episodes_file, unknown_episodes_file, n_buffer=50)
 
 def plot_rel_use():
-    plot_relative_usage('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/limited_demos/mab_alpha_2_0/short_files', 1350)
+    plot_relative_usage('/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/demo_cost_0_143/alpha_1_0/short_files', 1200)
 
 def plot_success_rate():
     folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/limited_demos/mab_alpha_2_0/short_files',
@@ -25,17 +25,15 @@ def plot_baseline_rate():
     plot_baseline_success_rate(folders, methods, 230)
 
 def plot_human_cost():
-    folders =  ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/alpha_0_5/short_files',
-                '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/alpha_1_0/short_files',
-                '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/alpha_2_0/short_files',
+    folders =  ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/demo_cost_0_2/alpha_0_5/short_files',
+                '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/demo_cost_0_2/alpha_1_0/short_files',
+                '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/demo_cost_0_2/alpha_2_0/short_files',
                 '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_baseline_mab/alpha_1_0/short_files',
-                '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/400demos/short_files',
+                '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/300demos/short_files',
                 '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_baseline_softmax/dtau_0_01/short_files']
 
-    #folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/200demos/short_files',
-    #            '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/300demos/short_files',
-    #            '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/400demos/short_files']
-    #folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/alpha_1_0']
+
+    #folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/400demos_repeat/short_files']
 
     #folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_baseline_ncb/alpha_1_0',
     #            '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/alpha_1_0']
@@ -50,19 +48,19 @@ def plot_human_cost():
     #            '/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_learner_mab/alpha_2_0']
     #folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/rl_only/single_file']
     methods = [r'$\it{contextual\ bandit\ (\alpha = 0.5)}$', r'$\it{contextual\ bandit\ (\alpha = 1)}$', r'$\it{contextual\ bandit\ (\alpha = 2)}$',
-                r'$\it{contextual\ bandit\ w.\ baseline\ (\alpha = 1)}$', r'$\it{human\ then\ learner\ (n_h = 400)}$', r'$\it{Boltzmann\ (\Delta \tau=0.002)}$', 'rl only', '', '']
-    folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/rl_only']
+                r'$\it{contextual\ bandit\ w.\ baseline\ (\alpha = 1)}$', r'$\it{human\ then\ learner\ (n_h = 300)}$', r'$\it{Boltzmann\ (\Delta \tau=0.002)}$', 'rl only', '', '']
+    #folders = ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/rl_only']
     plot_human_cost_sliding_window(folders, methods, num_episodes = 1200, cost_failure = 5.0)
 
 def shorten_files():
-    folders =  ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/limited_demos/human150_then_learner_eps_0']
+    folders =  ['/home/marcrigter/pCloudDrive/Development/LearnToManipulate/data/main_experiment/human_then_learner/300demos_repeat']
     create_short_files(folders)
 
 if __name__ == "__main__" :
     #shorten_files()
     #shorten_files()
     #plot_length_param()
-    plot_rel_use()
+    #plot_rel_use()
     #plot_baseline_rate()
-    #plot_human_cost()
-    plot_success_rate()
+    plot_human_cost()
+    #plot_success_rate()
--- src/learn_to_manipulate/plot.py
+++ src/learn_to_manipulate/plot.py
@@ -55,7 +55,6 @@ def plot_human_cost_sliding_window(folders, method_names, num_episodes = 500, co
 
         for file_path_index in range(len(files)):
             file_path = files[file_path_index]
-            print(file_path)
             file = open(file_path, 'rb')
             total_cost = 0.0
             sliding_window_mean_costs = []
@@ -141,6 +140,58 @@ def plot_ddpg_success_rate(folders, method_names, max_episodes = 500):
                         window_episodes += 1
                         if episode.result:
                             window_successes += 1.0
+                mean_success_rate = window_successes/window_episodes
+                sliding_window_mean_success_rate.append(mean_success_rate)
+            success_array[file_path_index, :] = sliding_window_mean_success_rate[0:max_episodes]
+            success_stddevs = np.std(success_array, axis=0)
+            success_means = np.mean(success_array, axis=0)
+            file_path_index += 1
+
+        plt.plot(range(len(success_means)), success_means, colors[folder_ind], linewidth = 3.0,  label = method_names[folder_ind])
+        plt.fill_between(range(len(success_means)), success_means-success_stddevs, success_means+success_stddevs, color = colors[folder_ind], alpha=.1)
+    plt.xlabel(r"$C_l$ Episode")
+    plt.ylabel("Success Rate")
+    plt.ylim([0.0, 1.0])
+    plt.xlim([0.0, float(max_episodes)])
+    plt.legend()
+    plt.title("sliding window of success rate for ddpg policy")
+    plt.show()
+
+def plot_limited_demos(folders, method_names, max_episodes = 500):
+    plt.close("all")
+    plt.rcParams.update({'font.size': 14})
+
+    # make sure this is correct!
+    human_demo_cost = 1.0
+    failure_cost = 3.0
+    colors = "rkcm"
+    window_half_width = 25
+
+    for folder_ind in range(len(folders)):
+        folder_path = folders[folder_ind]
+        labelled_flag = False
+        files = [join(folder_path, f) for f in listdir(folder_path) if isfile(join(folder_path, f))]
+        success_array = np.zeros([len(files), max_episodes])
+
+        for file_path_index in range(len(files)):
+            file_path = files[file_path_index]
+            file = open(file_path, 'rb')
+            successes = 0.0
+            sliding_window_mean_success_rate = []
+            success_list, demo_list = pickle.load(file)
+
+            for episode_index in range(len(success_list)):
+                window_successes = 0.0
+                window_episodes = 0.0
+
+                # loop through the window and tally success and failure
+                for i in range((episode_index - window_half_width), (episode_index + window_half_width + 1)):
+                    if (i < 0) or (i >= len(success_list)):
+                        continue
+                    result = success_list[i]
+                    window_episodes += 1
+                    if success_list[i]:
+                        window_successes += 1.0
                 mean_success_rate = window_successes/window_episodes
                 sliding_window_mean_success_rate.append(mean_success_rate)
             success_array[file_path_index, :] = sliding_window_mean_success_rate[0:max_episodes]
